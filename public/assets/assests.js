6651f15450fe1f377d2f693c07afada1d74fdfdc
DMSdivyamanas <98876605+DMSdivyamanas@users.noreply.github.com>
2024-11-11 12:39:05 +0530

    chore: started worl

---
+++ public/assets/assests.js
@@ -0,0 +1,35 @@
+import DotMenu from "./svg/Three_dot_menu.svg";
+import Add from "./svg/add.svg";
+import Backlog from "./svg/Backlog.svg";
+import Cancelled from "./svg/Cancelled.svg";
+import Display from "./svg/Display.svg";
+import Done from "./svg/Done.svg";
+import Down from "./svg/down.svg";
+import HighPriority from "./svg/Img - High Priority.svg";
+import LowPriority from "./svg/Img - Low Priority.svg";
+import MediumPriority from "./svg/Img - Medium Priority.svg";
+import NoPriority from "./svg/No-priority.svg";
+import InProgress from "./svg/in-progress.svg";
+import UrgentPriorityColor from "./svg/SVG - Urgent Priority colour.svg";
+import UrgentPriorityGrey from "./svg/SVG - Urgent Priority grey.svg";
+import ToDo from "./svg/To-do.svg";
+
+const Assets = {
+  DotMenu,
+  Add,
+  Backlog,
+  Cancelled,
+  Display,
+  Done,
+  Down,
+  HighPriority,
+  LowPriority,
+  MediumPriority,
+  NoPriority,
+  InProgress,
+  UrgentPriorityColor,
+  UrgentPriorityGrey,
+  ToDo,
+};
+
+export default Assets;
